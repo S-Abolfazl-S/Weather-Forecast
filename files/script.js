9560b4c89675f5a9885f0e7a82f2a2ff21c3a28e
s-abolfazl-s <sas57se21d1@gmail.com>
2021-10-31 15:34:23 +0330

    Content edited to be responsive

--- files/script.js
+++ files/script.js
@@ -3,25 +3,29 @@ $(() => {
     let btn = document.querySelector('#searchBtn');
     let input = document.querySelector('#searchBox>input');
     let li = document.querySelectorAll('nav>ul>li');
+
     $(window).on('resize load', e => {
-        if (window.innerWidth < 400) {
+        if (window.innerWidth < 450) {
+            // header
             $('#searchBox , #searchBox *').css({ width: '100%', margin: '5px 0' });
             $(btn).html('search');
-            // 
+            // navbar
             $(li).css({ 'width': '100%', 'margin': '0 0 10px 0' });
-            // 
-            $(li).children('a').removeClass('btn-outline-light')
-            $(li).children('a').addClass('btn-danger')
+            $(li).children('a').removeClass('btn-outline-light');
+            $(li).children('a').addClass('btn-danger');
+            // content 
+
         } else {
+            // header
             $(searchBox).css({ width: '50%', margin: 'auto' });
             $(btn).css({ width: 'auto', 'margin': 'auto auto auto 0' });
             $(input).css({ width: '70%', 'margin': 'auto 10px auto auto ' });
             $(btn).html('<i class="fa fa-search"></i>')
-            // 
+            // navbar
             $(li).css({ 'width': '150px', 'margin-left': '10px' });
-            //
             $(li).children('a').removeClass('btn-danger')
             $(li).children('a').addClass('btn-outline-light')
+            // content
 
         }
     });
